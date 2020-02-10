5a1ab82eec84e5ecc6213e3682fe5246d8b21768
Michael John Isip <mj.isip123@gmail.com>
2020-02-10 15:35:12 +0800

    update

--- src/components/info-bar/info-bar.js
+++ src/components/info-bar/info-bar.js
@@ -8,14 +8,14 @@ const InfoBar = ({room}) => (
             <img 
                 className="onlineIcon" 
                 src="https://raw.githubusercontent.com/adrianhajdin/project_chat_application/master/client/src/icons/onlineIcon.png" 
-                alt="online icon" />
+                alt="online" />
             <h3> { room } </h3>
         </div>
         <div className="rightInnerContainer">
             <a href="/">
                 <img 
                     src="https://raw.githubusercontent.com/adrianhajdin/project_chat_application/master/client/src/icons/closeIcon.png" 
-                    alt="close image" />
+                    alt="close" />
             </a>
         </div>
     </div>
